15c89e7611d0307906e22e68ca4a931f3e7f10b2
hr-sobuj <habibur.rahman@gigalogy.com>
2024-06-10 11:34:27 +0600

    blog post create and fetch api done

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,request,jsonify
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import SQLAlchemyError
 from dotenv import load_dotenv
@@ -16,6 +16,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200), nullable=False)
     profile = db.relationship("Profile", uselist=False, back_populates="user")
+    post = db.relationship("Post", backref="user", lazy=True)
 
 
 class Profile(db.Model):
@@ -23,6 +24,13 @@ class Profile(db.Model):
     bio = db.Column(db.String(200), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), unique=True)
     user = db.relationship("User", uselist=False, back_populates="profile")
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), unique=True)
 
 
 with app.app_context():
@@ -33,50 +41,101 @@ with app.app_context():
 def home():
     return "Homepage"
 
-@app.post('/create-user')
+
+@app.post("/create-user")
 def create_user():
     try:
-        data=request.json
-        name=data.get('name')
-        bio=data.get('bio')
+        data = request.json
+        name = data.get("name")
+        bio = data.get("bio")
 
-        user=User(name=name)
-        profile=Profile(bio=bio)
-        user.profile=profile
+        user = User(name=name)
+        profile = Profile(bio=bio)
+        user.profile = profile
 
         db.session.add(user)
         db.session.commit()
 
         if user.id is not None:
-            return jsonify({
-                'message':'User Added Successfully'
-            }), 201
+            return jsonify({"message": "User Added Successfully"}), 201
         else:
-             return jsonify({
-                'message':'User creation failed. Some error occurred on database'
-            }), 400
+            return (
+                jsonify(
+                    {"message": "User creation failed. Some error occurred on database"}
+                ),
+                400,
+            )
     except SQLAlchemyError as err:
-        return jsonify({
-                'message':'User creation failed. Some error occurred on server end.'
-            }), 500
+        return (
+            jsonify(
+                {
+                    "message": "User creation failed. Some error occurred on server end.",
+                    "Error": f"The error is {err}",
+                }
+            ),
+            500,
+        )
 
-@app.get('/users')
+
+@app.get("/users")
 def get_users():
     try:
-        users=User.query.all()
-        user_list=[]
+        users = User.query.all()
+        user_list = []
         for user in users:
-            user_list.append({
-                'id':user.id,
-                'name':user.name,
-                'bio':user.profile.bio
-            })
+            user_data = {
+                "id": user.id,
+                "name": user.name,
+                "bio": user.profile.bio,
+                "post": [],
+            }
+
+            for post in user.post:
+                post_data = {
+                    "id": post.id,
+                    "title": post.title,
+                    "description": post.description,
+                }
+
+                user_data['post'].append(post_data)
+            user_list.append(user_data)
+
         return jsonify(user_list), 200
     except Exception as err:
-        return jsonify({
-            'message':'Operation failed!',
-            'Error':f"The Error is {err}"
-        }), 500
+        return (
+            jsonify({"message": "Operation failed!", "Error": f"The Error is {err}"}),
+            500,
+        )
+
+
+@app.post("/create-post")
+def create_post():
+    data = request.json
+    username = data.get("username")
+    title = data.get("title")
+    description = data.get("description")
+
+    users = User.query.all()
+
+    for user in users:
+        if user.name == username:
+            post = Post(title=title, description=description)
+            post.user = user
+
+            db.session.add(post)
+            db.session.commit()
+
+            if post.id is not None:
+                return jsonify({"message": "Post added Successfully"}), 201
+            else:
+                return (
+                    jsonify(
+                        {
+                            "message": "Post creation failed. Some error occurred on database"
+                        }
+                    ),
+                    400,
+                )
 
 
 if __name__ == "__main__":
